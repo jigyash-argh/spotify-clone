e4d146d45a94a47219cd6823dd87ef7d5b3bd65c
jigyash-argh <realjigyash19@gmail.com>
2025-08-08 23:25:51 +0530

    basics done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './components/hersection/HeroSection'
 import Rightpart from './components/rightside/Rightpart'
+import Musicplayer from './components/hersection/Musicplayer'
 
 const App = () => {
   return (
     <div className='min-h-screen w-full bg-black'>
       <Navbar/>
       <HeroSection/>
-      <Rightpart/>
+      <Musicplayer/>
     </div>
   )
 }
--- src/components/hersection/HeroSection.jsx
+++ src/components/hersection/HeroSection.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Liberary from '../liberary/Liberary'
 import MiddlePart from './MiddlePart'
+import Rightpart from '../rightside/Rightpart'
 
 const HeroSection = () => {
   return (
-    <div>
+    <div className='flex justify-around'>
         <Liberary/>
         <MiddlePart/>
+        <Rightpart/>
     </div>
   )
 }
